34fb743e9437297c66e1a9c9511cbf2f6eabd65d
Abel Toth <abeltoth7@gmail.com>
2021-01-31 01:58:42 +0100

    Cors handler middleware added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const corsHandler = require('./middleware/cors-handler');
 const recipeRouter = require('./routers/recipe');
 
 const app = express();
 
+app.use(corsHandler);
 app.use(express.json());
 app.use(recipeRouter);
 
